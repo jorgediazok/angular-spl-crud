bcb6a771980e1d56702d89786056c1766adf2fb8
Jorge <jorgediazok@gmail.com>
2020-05-26 18:11:10 -0300

    i created the bucle for of employees

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Employee } from './models/employee';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-spl-crud';
+  employeeArray: Employee[] = [
+    { id: 1, name: 'Ryan', country: 'USA' },
+    { id: 2, name: 'Mumi', country: 'India' },
+    { id: 3, name: 'Hugo', country: 'Venezuela' },
+  ];
 }
